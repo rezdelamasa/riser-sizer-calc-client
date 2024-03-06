2348afd3023f9b6b20534cbd336b0289f1996b6f
rezdelamasa <rezdelamasa@gmail.com>
2024-03-06 16:03:01 -0500

    sidebar styles

--- src/App.vue
+++ src/App.vue
@@ -35,18 +35,71 @@ header {
   z-index: 10;
   height: 100px;
 }
-
-.wrapper {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  max-width: 100vw;
-  overflow-x: hidden;
-  display: flex;
-  flex-direction: column;
+h2 {
+  font-style: italic;
 }
 
-.sidebarComp {
+.router-link-active {
+  background: #333;
+}
+
+.router-link-active:deep(*) {
+  color: white;
+}
+
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: grid;
+  grid-template-columns: 240px 1fr;
+  grid-template-rows: 1fr;
+  grid-column-gap: 0px;
+  grid-row-gap: 0px; 
+}
+
+.link {
+  overflow: hidden;
+  border-radius: 6px;
+}
+
+.link__button {
+  width: 100%;
+  height: 3rem;
+  color: #333;
+}
+
+.link:hover {
+  background-color: #333;
+}
+
+.link__button:deep(.p-button-label) {
+  text-align: left;
+  font-weight: normal;
+  margin-left: 0.5rem;
+}
+
+.link__button:deep(.p-button-icon) {
+  font-size: 1.15rem;
+}
+
+.link__button:hover:deep(*) {
+  color: white;
+}
+
+aside {
+  max-width: 320px;
+  display: flex;
+  padding: 20px;
+  flex-direction: column;
+  align-items: left;
+}
+
+.links {
+  margin-top: 3rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 }
 
 .content {
