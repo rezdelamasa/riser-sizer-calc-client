9b5a3b6dd318f35579fd96b0129ad68d109f6103
rezdelamasa <rezdelamasa@gmail.com>
2024-03-13 14:28:52 -0400

    use addToFavorites from store

--- src/components/FixtureSelector.vue
+++ src/components/FixtureSelector.vue
@@ -72,7 +72,7 @@ const addToFavorites = (fixture) => {
                         <p>{{ fixture.fixtureType }}</p>
                         <p>{{ fixture.occupancy }}</p>
                     </div>
-                    <Button @click.stop="branchFixturesStore.addToFavorites(fixture)" class="favorite-button"
+                    <Button @click.stop="addToFavorites(fixture)" class="favorite-button"
                         :icon="{ 'pi pi-star-fill': isFavorited(fixture), 'pi pi-star': !isFavorited(fixture) }"
                         text rounded></Button>
                 </Button>
