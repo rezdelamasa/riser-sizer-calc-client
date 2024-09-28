45e6fe76acd391fd0eb584c8e3b6d6817efa6d26
rezdelamasa <rezdelamasa@gmail.com>
2024-09-28 00:17:33 -0400

    create correct form with form state

--- src/views/RiserView.vue
+++ src/views/RiserView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, reactive } from 'vue'
 import { useBranchesStore } from "@/stores/branches"
 import { useRoute } from 'vue-router';
 import BackLink from '@/components/BackLink.vue';
@@ -11,6 +11,11 @@ const createDialogVisible = ref(false)
 
 const backLink = ref("")
 const { params, path } = useRoute()
+
+const form = reactive({
+    label: null,
+    startingFloor: null,
+})
 
 onMounted(async () => {
     await branchesStore.getBranches(params.riser_id)
@@ -63,29 +68,14 @@ const toggleCreateDialog = () => {
         </div>
         <Dialog v-model:visible="createDialogVisible" modal header="Create Riser" :draggable="false" style="width: 80vw; max-width: 720px;">
             <div class="flex flex-column align-items-fill gap-3 mb-4">
-                <label for="project_name" class="font-semibold w-100">Project Name</label>
-                <InputText v-model="form.name" id="project_name" class="flex-auto" autocomplete="off" required />
-                <span v-if="v$.name.$error" class="text-red-500">{{ v$.name.required.$message }}</span>
+                <label for="label" class="font-semibold w-100">Branch Label</label>
+                <InputText v-model="form.label" id="label" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.label.$error" class="text-red-500">{{ v$.label.required.$message }}</span>
             </div>
             <div class="flex flex-column align-items-fill gap-3 mb-4">
-                <label for="address" class="font-semibold">Address</label>
-                <InputText v-model="form.address" id="address" class="flex-auto" autocomplete="off" required />
-                <span v-if="v$.address.$error" class="text-red-500">{{ v$.address.required.$message }}</span>
-            </div>
-            <div class="flex flex-column align-items-fill gap-3 mb-4">
-                <label for="building_type" class="font-semibold">Building Type</label>
-                <InputText v-model="form.type" id="building_type" class="flex-auto" autocomplete="off" required />
-                <span v-if="v$.type.$error" class="text-red-500">{{ v$.type.required.$message }}</span>
-            </div>
-            <div class="flex flex-column align-items-fill gap-3 mb-4">
-                <label for="floors" class="font-semibold">Number of Floors</label>
-                <InputNumber v-model="form.floors" id="floors" class="flex-auto" autocomplete="off" required />
-                <span v-if="v$.floors.$error" class="text-red-500">{{ v$.floors.required.$message }}</span>
-            </div>
-            <div class="flex flex-column align-items-fill gap-3 mb-4">
-                <label for="risers" class="font-semibold">Number of Risers</label>
-                <InputNumber v-model="form.risers" id="floors" class="flex-auto" autocomplete="off" required />
-                <span v-if="v$.risers.$error" class="text-red-500">{{ v$.risers.required.$message }}</span>
+                <label for="startingFloor" class="font-semibold">Starting Floor</label>
+                <InputNumber v-model="form.startingFloor" id="startingFloor" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.startingFloor.$error" class="text-red-500">{{ v$.startingFloor.required.$message }}</span>
             </div>
             <div class="flex justify-content-end gap-2">
                 <Button type="button" label="Cancel" severity="secondary" @click="toggleCreateDialog"></Button>
