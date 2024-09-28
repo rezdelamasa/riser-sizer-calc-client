b3cf99ba5b6997ba1d24aa48b5175f0ec5e23583
rezdelamasa <rezdelamasa@gmail.com>
2024-09-28 00:00:39 -0400

    create postProject action

--- src/stores/projects.ts
+++ src/stores/projects.ts
@@ -1,5 +1,6 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { v4 as uuidv4 } from "uuid";
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref([])
@@ -18,7 +19,38 @@ export const useProjectsStore = defineStore('projects-store', () => {
     }
   }
 
+  const postProject = async (projectObj) => {
+    const { name, address, type, floors, risers } = projectObj; 
+
+    const body = {
+      id: uuidv4(),
+      name,
+      address,
+      type,
+      floors,
+      risers
+    }
+
+    try {
+      await fetch(`http://localhost:3000/projects`, 
+      {
+        method: "POST",
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        projects.value.push(data);
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
 
-  return { projects, getProjects }
+
+  return { projects, getProjects, postProject }
 })
