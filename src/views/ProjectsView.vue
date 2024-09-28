524bd20320c05c8718e7b7c4aaf3817a4c083596
rezdelamasa <rezdelamasa@gmail.com>
2024-09-28 00:00:57 -0400

    create dialog

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -43,6 +43,37 @@ const projects = computed(() => {
                 </template>
             </Card>
         </div>
+        <Dialog v-model:visible="createDialogVisible" modal header="Create Riser" :draggable="false" style="width: 80vw; max-width: 720px;">
+            <div class="flex flex-column align-items-fill gap-3 mb-4">
+                <label for="project_name" class="font-semibold w-100">Project Name</label>
+                <InputText v-model="form.name" id="project_name" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.name.$error" class="text-red-500">{{ v$.name.required.$message }}</span>
+            </div>
+            <div class="flex flex-column align-items-fill gap-3 mb-4">
+                <label for="address" class="font-semibold">Address</label>
+                <InputText v-model="form.address" id="address" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.address.$error" class="text-red-500">{{ v$.address.required.$message }}</span>
+            </div>
+            <div class="flex flex-column align-items-fill gap-3 mb-4">
+                <label for="building_type" class="font-semibold">Building Type</label>
+                <InputText v-model="form.type" id="building_type" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.type.$error" class="text-red-500">{{ v$.type.required.$message }}</span>
+            </div>
+            <div class="flex flex-column align-items-fill gap-3 mb-4">
+                <label for="floors" class="font-semibold">Number of Floors</label>
+                <InputNumber v-model="form.floors" id="floors" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.floors.$error" class="text-red-500">{{ v$.floors.required.$message }}</span>
+            </div>
+            <div class="flex flex-column align-items-fill gap-3 mb-4">
+                <label for="risers" class="font-semibold">Number of Risers</label>
+                <InputNumber v-model="form.risers" id="floors" class="flex-auto" autocomplete="off" required />
+                <span v-if="v$.risers.$error" class="text-red-500">{{ v$.risers.required.$message }}</span>
+            </div>
+            <div class="flex justify-content-end gap-2">
+                <Button type="button" label="Cancel" severity="secondary" @click="toggleCreateDialog"></Button>
+                <Button type="button" label="Save" @click="createProject"></Button>
+            </div>
+        </Dialog>
     </main>
 </template>
 
